062017e1e685921ab078c3665ef60986636f1f3d
YASH RASTOGI <rastogiy60@gmail.com>
2026-03-01 12:55:39 +0000

    container config done

---
+++ 01_basics/Variable.js
@@ -0,0 +1,10 @@
+const accountId =7777
+let accountEmail = "yash@1234gmail.com"
+var accountPass = "mahi"
+accountCity = "JAIPUR"
+accountEmail= "yash@234gmail.com"
+accountPass=7890
+console.table([accountEmail,accountPass])
+
+
+console.log(accountId);
