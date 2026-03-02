4119160f72896c850b9b687c7598ac1a74b7a777
YASH RASTOGI <rastogiy60@gmail.com>
2026-03-02 16:59:34 +0000

    datatype

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+// use all the JS code as newer version 
+// alert(3+3) we are using nodejs not brower
+
+console.log(3+3);
+console.log("yash")
+// let = "yash"//string
+let age  = 20//int
+// let isLoggedin  = false//boolean
+//null = standalone value
+//undefined = not asigned the value 
+// symbol = unique 
+console.log(typeof"yash")
+console.log(typeof "age")
+console.log(typeof"10")
+console.log(typeof null);
+console.log(typeof undefined);//undefined bcz it is a type
+
+
+
+
