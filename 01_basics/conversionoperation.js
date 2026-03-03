4b6692c28da071e2eb922f4891f782a8ca6c4387
YASH RASTOGI <rastogiy60@gmail.com>
2026-03-03 13:37:27 +0000

    Conversion of Operation

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,25 @@
+// let score = "77"
+let score ="33ABC"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber = Number (score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);//nan = not a number
+// score null then the valueofNumber is = 0
+
+//33 = 33
+// 33abc= nan
+//true = 1; fals = 0;
+let isLoggedin = 1
+let isBoolean =Boolean (isLoggedin)
+console.log(isBoolean)
+// 1= ture
+// 0= false
+// ""= false
+// "yash"= true
+let someno = 33
+let Bac = String(someno)
+console.log(Bac)
+console.log(typeof Bac)
+
