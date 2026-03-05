4493318e03af4130181ed2ba329b6556d7a1f2f3
YASH RASTOGI <rastogiy60@gmail.com>
2026-03-05 18:32:41 +0000

    operations

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -1,25 +1,41 @@
 // let score = "77"
-let score ="33ABC"
-console.log(typeof score);
-console.log(typeof(score));
+// let score ="33ABC"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueinNumber = Number (score)
-console.log(typeof valueinNumber);
-console.log(valueinNumber);//nan = not a number
+// let valueinNumber = Number (score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);//nan = not a number
 // score null then the valueofNumber is = 0
 
 //33 = 33
 // 33abc= nan
 //true = 1; fals = 0;
-let isLoggedin = 1
-let isBoolean =Boolean (isLoggedin)
-console.log(isBoolean)
-// 1= ture
-// 0= false
-// ""= false
-// "yash"= true
-let someno = 33
-let Bac = String(someno)
-console.log(Bac)
-console.log(typeof Bac)
+// let isLoggedin = 1
+// let isBoolean =Boolean (isLoggedin)
+// console.log(isBoolean)
+// // 1= ture
+// // 0= false
+// // ""= false
+// // "yash"= true
+// let someno = 33
+// let Bac = String(someno)
+// console.log(Bac)
+// console.log(typeof Bac)
+// operation 
+let value = 4
+let negvalue = -value
+// console.log(negvalue);
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2%3)
+// console.log(4%2);
+// console.log(2%4);
+
+let str = "yash"
+let str2 = " rastogi"
+let str3  = str +str2
+console.log(str+str2)
+console.log(str3);
+ 
 
